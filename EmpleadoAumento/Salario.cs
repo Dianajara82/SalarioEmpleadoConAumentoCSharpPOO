0c1b44249433bb70d01ba281c64e7b5e50de4830
Diana Arismendi <dianajara82@gmail.com>
2020-09-20 19:37:13 -0500

    Ejercicio de salario de empleado con aumento en C#, usando POO

---
+++ EmpleadoAumento/Salario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmpleadoAumento
+{
+    public class Salario : Principal
+    {
+        private double total;
+        private double aumento;
+        public void calcularSalario()
+        {
+            if (this.getAntiguedad() == "5 años")
+            {
+                this.aumento = this.getSalario() * 0.3;
+                this.total = this.aumento + this.getSalario();
+
+            }
+            else if (this.getAntiguedad() == "7 años")
+            {
+                this.aumento = this.getSalario() * 0.4;
+                this.total = this.aumento + this.getSalario();
+
+            }
+            else if (this.getAntiguedad() == "10 años")
+            {
+                this.aumento = this.getSalario() * 0.45;
+                this.total = this.aumento + this.getSalario();
+
+            }
+            else
+            {
+                this.aumento = this.getSalario() * 0.5;
+                this.total = this.aumento + this.getSalario();
+            }
+
+        }
+
+        public double getTotal()
+        {
+            return this.total;
+        }
+        public double getAumento()
+        {
+            return this.aumento;
+        }
+    }
+}
